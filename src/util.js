1d5565fd262ea7d6e09a7d5f4009948e7db57d5b
Roy Hewitt <roy_hewitt@fws.gov>
2017-02-01 13:13:54 -0500

    Add node.matches() polyfill

--- src/util.js
+++ src/util.js
@@ -1,6 +1,22 @@
 (function () {
   'use strict';
 
+  // Polyfill node.matches() -> https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
+  if (!Element.prototype.matches) {
+    Element.prototype.matches =
+      Element.prototype.matchesSelector ||
+      Element.prototype.mozMatchesSelector ||
+      Element.prototype.msMatchesSelector ||
+      Element.prototype.oMatchesSelector ||
+      Element.prototype.webkitMatchesSelector ||
+      function(s) {
+        var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+          i = matches.length;
+        while (--i >= 0 && matches.item(i) !== this) {}
+        return i > -1;
+      };
+  }
+
   var _ = {
     each: require('lodash.forEach'),
     filter: require('lodash.filter'),
